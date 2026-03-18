9c44efd7e7c21b1d27744726c875acbfaf98adeb
Anthony <104888620+ItsHiraeths@users.noreply.github.com>
2026-03-18 10:32:46 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if "hello" in message.content.lower():
+        await message.channel.send("Hello 👋")
+
+    await bot.process_commands(message)
+
+bot.run(os.getenv("TOKEN"))
